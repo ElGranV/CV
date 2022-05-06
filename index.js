5ef5ea3c6a51e151b15c3cd40709665d84501251
ElGranV <ethan.rinaldo971@gmail.com>
2022-05-06 13:50:58 -0400

    fix gallery images problems

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ for (let i = 0; i<app_images.length; i++)
     app_images[i].classList.add("shadow")
 }
 
-let button_panel = document.getElementById("trigger-panel");
-button_panel.addEventListener("click", ()=>{
-    
+let image_panel = document.getElementById("image-panel");
+image_panel.addEventListener("click", ()=>{
+    image_panel.style.display = "none"
+})
+
+let trigger_panel = document.getElementById("trigger-panel");
+trigger_panel.addEventListener("click", ()=>{
+    image_panel.style.display = "flex"
 })
