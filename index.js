996a39d8a9350f46cda897f2f9d2e40882215b11
ElGranV <ethan.rinaldo971@gmail.com>
2022-05-06 13:16:12 -0400

    image gallery

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ for (let i = 0; i<app_images.length; i++)
 {
     app_images[i].classList.add("shadow")
 }
+
+let button_panel = document.getElementById("trigger-panel");
+button_panel.addEventListener("click", ()=>{
+    
+})
