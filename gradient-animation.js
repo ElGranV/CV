5bc1351166a2233fa08a67be8cca8827ea15eba8
ElGranV <karnak75e@gmail.com>
2024-05-20 12:54:02 -0400

    Image rencontre & parcours VPE

--- gradient-animation.js
+++ gradient-animation.js
@@ -1,9 +1,10 @@
-let header = document.getElementById("header");
-header.addEventListener("mouseover", (e)=> {
-    rect = e.currentTarget.getBoundingClientRect();
-    let x = e.clientX - rect.left; //x position within the element.
-    let y = e.clientY - rect.top;  //y position within the element.
-    let percentage = Math.round(100*x/rect.width);
+// Code pour que le gradient suive la souris
+// let header = document.getElementById("header");
+// header.addEventListener("mouseover", (e)=> {
+//     rect = e.currentTarget.getBoundingClientRect();
+//     let x = e.clientX - rect.left; //x position within the element.
+//     let y = e.clientY - rect.top;  //y position within the element.
+//     let percentage = Math.round(100*x/rect.width);
     
-    // header.style.background = "linear-gradient(70deg, darkblue "+ percentage.toString() + "%,#006a8f,#00bf9f "+ (percentage+20).toString() +"%, lightgreen)"
-})
+//     // header.style.background = "linear-gradient(70deg, darkblue "+ percentage.toString() + "%,#006a8f,#00bf9f "+ (percentage+20).toString() +"%, lightgreen)"
+// })
